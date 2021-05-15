2c1f7178b15050f865538188c396f121322d66c2
Alexander Stepanov <stepa113@mail.ru>
2021-05-15 12:00:45 +0300

    Изменил Filter.java

--- src/main/java/com/gridnine/testing/filter/Filter.java
+++ src/main/java/com/gridnine/testing/filter/Filter.java
@@ -20,24 +20,25 @@ public class Filter {
 
 
     public List<Flight> checkForRulesAndRemoveFlightIfNeed(List<Flight> flightsListForFiltering){
-        List <Flight> flightsListAfterFiltering = new LinkedList<>(flightsListForFiltering);
-        List <Integer> indexForRemoving = new ArrayList<>();
+        List <Flight> flightsListAfterFiltering = new LinkedList<>(flightsListForFiltering);  //LinkedList для быстрого исполнения remove
 
-        for (int i = 0; i < rulesForFilteringList.size() ; i++) {      //Rules
+        for (int i = 0; i < rulesForFilteringList.size() ; i++) {    //Rules
             Rule currentRule = rulesForFilteringList.get(i);
-            for (int j = flightsListAfterFiltering.size()-1; j >=0 ; j--) {  //Flights
+
+            for (int j = flightsListForFiltering.size()-1; j >=0 ; j--) {  //Flights
                 Flight currentFlight = flightsListForFiltering.get(j);
+
                 if (!currentRule.checkForRule(currentFlight)){               //Если данный перелет не соответствует правилам то мы можем его удалить
                     if (flightsListAfterFiltering.remove(currentFlight)) {   //Если до этого он не был удален другим правилом, то удалим его
                         System.out.println("После фильтрации согласно правилу " + currentRule.getClass().getSimpleName() + " :\n"
                                 + "Удален перелёт - " + currentFlight + "\n"
                                 + "----------------------------------------------");
                     }
+
                 }
             }
-        }
-        System.out.println(indexForRemoving);
 
+        }
         return flightsListAfterFiltering;
     }
 }
